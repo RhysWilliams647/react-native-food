c2d2e8b6eedd5222d7d563f7d48bc32925659934
RhysWilliams647 <rhyswilliams647@googlemail.com>
2020-04-12 18:39:59 +0100

    Initial react food app

---
+++ App.js
@@ -0,0 +1,22 @@
+//  yelp client id: vzvAHgAuYADPpaONrbo04w
+//  yelp api key: 78fxxc5jon2aNjVV7yOndteY8_6iNoMxn13IybT-q7H2QmRc1bRHAYiNm3DK_a9XkObRQMYmmA2G7nBIKDENKvH3CeGTFEu5dPx0J2s6DQEwve5dDDukdxmqBlCTXnYx
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
